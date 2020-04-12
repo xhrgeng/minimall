d214c37b17043e34381a2e841e71f7229dc13767
genggeng <Want_me163.com>
2020-04-12 19:01:46 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import axios from 'axios'
 import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   data () {
-    return {
-      data:""
-    }
+    
   },
   components: {
-    HelloWorld
+   
   },
   mounted () {
-    //let url="网址"
-    //axios.get(url).then(()=>{
-
-    //})
-    jsonp(url,(err,res)=>{
-      let result = res;
-      this.data=result
-
-    })
+ 
   }
 }
 </script>
